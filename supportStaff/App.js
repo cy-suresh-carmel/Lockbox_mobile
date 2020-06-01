6328b91bceba051d1f13812b564fdfeda9d6e222
unknown <https://niran-alex@bitbucket.org>
2020-06-01 18:04:01 +0530

    Task #48238 Push Notification added to Support Staff App

--- supportStaff/App.js
+++ supportStaff/App.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react';
 import Navig from './Navig';
 import Splash from './Splash';
+import {fcmService} from './src/PushNotification/FCMService';
+import {localNotificationService} from './src/PushNotification/LocalNotificationService';
+
+
 export default class Main extends Component{
 constructor(props){
 super(props);
@@ -8,6 +12,39 @@ this.state={currentScreen:'Splash'};
 setTimeout(()=>{
 this.setState({currentScreen:'HomeScreen'})
 }, 3000)
+}
+
+componentDidMount() {
+    fcmService.registerAppWithFCM()
+        fcmService.register(onRegister,  onNotification, onOpenNotification)
+        localNotificationService.configure(onOpenNotification)
+
+        function onRegister(token) {
+            console.log("[App] onRegister:", token)
+        }
+        function onNotification(notify){
+            console.log("[App] onNotification:",notify)
+            const options = {
+                soundName: 'default',
+                playSound: true
+            }
+            localNotificationService.showNotification(
+                0,
+                notify.title,
+                notify.body,
+                notify,
+                options
+            )
+        }
+        function onOpenNotification(notify){
+            console.log("[App] onOpenNotification:",notify)
+            alert("Open Notification:"+ notify.body)
+        }
+        return () => {
+            console.log("[App] unRegister")
+            fcmService.unRegister()
+            localNotificationService.unregister()
+        }
 }
 render(){
 const { currentScreen } = this.state
